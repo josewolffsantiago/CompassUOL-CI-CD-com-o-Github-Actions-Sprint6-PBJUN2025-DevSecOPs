129c6ef4a422746173c738c023052e4556f52d50
José_Gustavo_Santiago <jose.gustavo@estudante.ifmt.edu.br>
2025-09-30 15:45:21 -0400

    Grafana teste: talvez?

--- hello-app/main/main.py
+++ hello-app/main/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from starlette.responses import Response
+
 app = FastAPI()
+hits = Counter("hits", "Number of hits to the root")
+
 @app.get("/")
 async def root():
     return {"message": "Hello World v3"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
+
